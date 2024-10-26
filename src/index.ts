77e15df3662b1c8a89f5187994021ca8155f053d
Wing Lee <44917547+wingleeio@users.noreply.github.com>
2024-10-26 13:59:40 -0700

    add fix to connect middleware

--- src/index.ts
+++ src/index.ts
@@ -1,111 +1,122 @@
 import { join } from "node:path";
 import { type AppLoadContext, createRequestHandler } from "@remix-run/node";
-import { Elysia } from "elysia";
+import { Elysia, type InferContext } from "elysia";
 import type { Context } from "elysia/context";
 import type { InlineConfig, ViteDevServer } from "vite";
 
 export type GetLoadContext = (
-	context: Context,
+    context: Context,
 ) => AppLoadContext | Promise<AppLoadContext>;
 
 export interface RemixOptions {
-	/**
-	 * in `development` mode it starts `vite` and in `production` it just served like static.
-	 *
-	 * @default process.env.NODE_ENV || "development"
-	 */
-	mode?: "development" | "production";
-	/**
-	 * The base path for the Remix app.
-	 * This should match the `basename` in your `vite` config.
-	 *
-	 * @default "/"
-	 */
-	basename?: string;
-	/**
-	 * The directory where the Remix app is built.
-	 * This should match the `buildDirectory` directory in your `vite` config.
-	 *
-	 * @default "build"
-	 */
-	buildDirectory?: string;
-	/**
-	 * The Remix server output filename.
-	 * This should match the `serverBuildFile` filename in your `vite` config.
-	 *
-	 * @default "index.js"
-	 */
-	serverBuildFile?: string;
-	/**
-	 * Configure `vite` server in `development` mode
-	 */
-	vite?: InlineConfig;
+    /**
+     * in `development` mode it starts `vite` and in `production` it just served like static.
+     *
+     * @default process.env.NODE_ENV || "development"
+     */
+    mode?: "development" | "production";
+    /**
+     * The base path for the Remix app.
+     * This should match the `basename` in your `vite` config.
+     *
+     * @default "/"
+     */
+    basename?: string;
+    /**
+     * The directory where the Remix app is built.
+     * This should match the `buildDirectory` directory in your `vite` config.
+     *
+     * @default "build"
+     */
+    buildDirectory?: string;
+    /**
+     * The Remix server output filename.
+     * This should match the `serverBuildFile` filename in your `vite` config.
+     *
+     * @default "index.js"
+     */
+    serverBuildFile?: string;
+    /**
+     * Configure `vite` server in `development` mode
+     */
+    vite?: InlineConfig;
 
-	/**
-	 * A function that returns the value to use as `context` in route `loader` and
-	 * `action` functions.
-	 *
-	 * You can use declaration merging for type it correctly https://www.typescriptlang.org/docs/handbook/declaration-merging.html
-	 */
-	getLoadContext?: GetLoadContext;
+    /**
+     * A function that returns the value to use as `context` in route `loader` and
+     * `action` functions.
+     *
+     * You can use declaration merging for type it correctly https://www.typescriptlang.org/docs/handbook/declaration-merging.html
+     */
+    getLoadContext?: GetLoadContext;
 }
 
 export async function remix(options?: RemixOptions) {
-	const cwd = process.env.REMIX_ROOT ?? process.cwd();
-	const mode = options?.mode ?? process.env.NODE_ENV ?? "development";
-	const buildDirectory = join(cwd, options?.buildDirectory ?? "build");
-	const serverBuildPath = join(
-		buildDirectory,
-		"server",
-		options?.serverBuildFile ?? "index.js",
-	);
+    const cwd = process.env.REMIX_ROOT ?? process.cwd();
+    const mode = options?.mode ?? process.env.NODE_ENV ?? "development";
+    const buildDirectory = join(cwd, options?.buildDirectory ?? "build");
+    const serverBuildPath = join(
+        buildDirectory,
+        "server",
+        options?.serverBuildFile ?? "index.js",
+    );
 
-	const elysia = new Elysia({
-		name: "elysia-remix",
-		seed: options,
-	});
+    const elysia = new Elysia({
+        name: "elysia-remix",
+        seed: options,
+    });
 
-	let vite: ViteDevServer | undefined;
+    let vite: ViteDevServer | undefined;
 
-	if (mode !== "production") {
-		vite = await import("vite").then((vite) => {
-			return vite.createServer({
-				...options?.vite,
-				server: {
-					...options?.vite?.server,
-					middlewareMode: true,
-				},
-			});
-		});
-	}
+    if (mode !== "production") {
+        vite = await import("vite").then((vite) => {
+            return vite.createServer({
+                ...options?.vite,
+                server: {
+                    ...options?.vite?.server,
+                    middlewareMode: true,
+                },
+            });
+        });
+    }
 
-	if (vite) {
-		elysia.use(
-			(await import("elysia-connect-middleware")).connect(vite.middlewares),
-		);
-	} else {
-		const clientDirectory = join(buildDirectory, "client");
-		const glob = new Bun.Glob(`${clientDirectory}/**`);
-		for (const path of glob.scanSync()) {
-			elysia.get(
-				path.substring(clientDirectory.length), 
-				() => new Response(Bun.file(path))
-			);
-		}
-	}
+    let hooks = {};
 
-	elysia.all("*", async function processRemixSSR(context) {
-		const handler = createRequestHandler(
-			vite
-				? await vite.ssrLoadModule("virtual:remix/server-build")
-				: await import(serverBuildPath),
-			mode,
-		);
+    if (vite) {
+        const { connectToWeb } = await import("connect-to-web");
+        hooks = {
+            beforeHandle: ({ request }: InferContext<typeof elysia>) => {
+                return connectToWeb((req, res, next) => {
+                    vite.middlewares(req, res, next);
+                })(request);
+            },
+        };
+    } else {
+        const clientDirectory = join(buildDirectory, "client");
+        const glob = new Bun.Glob(`${clientDirectory}/**`);
+        for (const path of glob.scanSync()) {
+            elysia.get(
+                path.substring(clientDirectory.length),
+                () => new Response(Bun.file(path)),
+            );
+        }
+    }
 
-		const loadContext = await options?.getLoadContext?.(context);
+    elysia.all(
+        "*",
+        async function processRemixSSR(context) {
+            const handler = createRequestHandler(
+                vite
+                    ? await vite.ssrLoadModule("virtual:remix/server-build")
+                    : await import(serverBuildPath),
+                mode,
+            );
 
-		return handler(context.request, loadContext);
-	});
+            const loadContext = await options?.getLoadContext?.(context);
 
-	return elysia;
+            return handler(context.request, loadContext);
+        },
+        hooks,
+    );
+
+    return elysia;
 }
